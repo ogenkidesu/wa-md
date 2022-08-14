57ba2034345967241094ed9f765bdba8e713d070
ogenkidesu <89525255+ogenkidesu@users.noreply.github.com>
2022-08-14 14:35:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.owner =
 
   // [number, dia creator/owner?, dia developer?]
 // Put your number here
-global.mods = [] // Want some help?
+global.mods = [6283894435611] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
